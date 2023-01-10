6dfdcd8cd92e360de5fe27e1f74258ff6b688b6f
ZmMatvey <115418268+ZmMatvey@users.noreply.github.com>
2023-01-10 21:17:59 +0300

    Add files via upload

--- source/repos/OpenGLTest2022/src/graphics/rendering/shader.cpp
+++ source/repos/OpenGLTest2022/src/graphics/rendering/shader.cpp
@@ -4,9 +4,18 @@ unsigned int shader::get_id() const {
 	return id;
 }
 
-shader::shader() {}
-shader::shader(const char* vertexShaderPath, const char* fragmentShaderPath) {
+shader::shader() : id(0), copy(true) {}
+shader::shader(const char* vertexShaderPath, const char* fragmentShaderPath) : copy(false) {
 	generate(vertexShaderPath, fragmentShaderPath);
+}
+
+shader::shader(const shader& a) : id(a.id), copy(true) {}
+
+shader& shader::operator=(const shader& a) {
+	shader b;
+	b.id = a.id;
+	b.copy = true;
+	return b;
 }
 
 void shader::generate(const char* vertexShaderPath, const char* fragmentShaderPath) {
@@ -45,38 +54,55 @@ std::string shader::loadShaderSrc(const char* filename) const {
 	file.open(filename);
 
 	if (file.is_open()) {
-		buf << file.rdbuf(); //?
+		buf << file.rdbuf(); 
 		ret = buf.str();
 	}
 	else {
 		std::cout << "error open " << filename << std::endl;
+		throw(std::logic_error("error open"));
 	}
 
 	file.close();
 
 	return ret;
 }
+
 GLuint shader::compileShader(const char* filepath, GLenum type) const {
 	int success;
 	char infoLog[512];
 
-	GLuint ret = glCreateShader(type);
-	std::string shaderSrc = loadShaderSrc(filepath);
-	const GLchar* shader = shaderSrc.c_str();
-	glShaderSource(ret, 1, &shader, NULL);
-	glCompileShader(ret);
+	GLuint ret = glCreateShader(type); //создаёт индекс для данного типа шейдера
+	std::string shaderSrc = loadShaderSrc(filepath); //переводит шейдер в строку
+	const GLchar* shader = shaderSrc.c_str(); //переводит строку в массив const char
+
+	//1 - к чему прикрепляем, 2 - количество строк, 3 - имя массива const char, 4 - массив длин строк
+	glShaderSource(ret, 1, &shader, NULL); //прикрепляем шейдер к сгенерированному индексу
+	glCompileShader(ret); //компилирует шейдер в ассемблере
 
 	//catch error
-	glGetShaderiv(ret, GL_COMPILE_STATUS, &success);
+	//1 - id шейдера, 2 - на какой статус выполнения хотим проверить, 3 - возращает 1 если действие было выполнено, иначе 0
+	glGetShaderiv(ret, GL_COMPILE_STATUS, &success); 
 	if (!success) {
+		//1 - id шейдера, 2 - максимальный размер кода ошибки, 3 - возращает размер кода ошибки, 4 - куда записывается код ошибки
 		glGetShaderInfoLog(ret, 512, NULL, infoLog);
-		std::cout << "Error with fragment shader comp.:" << std::endl << infoLog << std::endl;
+		std::cout << "Error with shader("<< type <<") compile: " << std::endl << infoLog << std::endl;
+		throw(std::logic_error("Error with shader compile"));
 	}
 
 	return ret;
 }
 
+shader::~shader() {
+	if (!copy) {
+		glDeleteProgram(id);
+	}
+}
+
 //передача матрицы name в vertex_shader(id), как uniform
+void shader::setMat3(const std::string& name, glm::mat3 value) const {
+	glUniformMatrix3fv(glGetUniformLocation(id, name.c_str()), 1, GL_FALSE, glm::value_ptr(value));
+}
+
 void shader::setMat4(const std::string& name, glm::mat4 value) const {
 	glUniformMatrix4fv(glGetUniformLocation(id, name.c_str()), 1, GL_FALSE, glm::value_ptr(value));
 }
--- source/repos/OpenGLTest2022/src/graphics/rendering/shader.h
+++ source/repos/OpenGLTest2022/src/graphics/rendering/shader.h
@@ -3,8 +3,7 @@
 
 #include <iostream>
 #include <fstream>
-#include <sstream> //?
-#include <streambuf> //?
+#include <sstream> 
 
 #define GLFW_INCLUDE_NONE
 #include <glad/glad.h> 
@@ -18,11 +17,17 @@ class shader final
 private:
 	unsigned int id;
 
+	bool copy;
+
 public:
 	unsigned int get_id() const;
 
 	shader();
 	shader(const char* vertexShaderPath, const char* fragmentShaderPath);
+	
+	shader(const shader& a);
+	shader& operator=(const shader& a);
+	~shader();
 
 	void generate(const char* vertexShaderPath, const char* fragmentShaderPath);
 
@@ -31,6 +36,7 @@ public:
 	std::string loadShaderSrc(const char* filename) const;
 	GLuint compileShader(const char* filepath, GLenum type) const;
 
+	void setMat3(const std::string& name, glm::mat3 value) const;
 	void setMat4(const std::string& name, glm::mat4 val) const;
 	void setInt(const std::string& name, int value) const;
 	void setFloat(const std::string& name, float value) const;
